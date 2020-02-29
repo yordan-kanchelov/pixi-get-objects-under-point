d1604219df32473848b545068367e8e2d0ea5196
Yordan Kanchelov <yordan.kanchelov@gmail.com>
2020-02-29 15:53:32 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+[![Netlify Status](https://api.netlify.com/api/v1/badges/14e7ef42-5c90-44c8-a7ec-0b6e20c59735/deploy-status)](https://pixi-typescript-boilerplate.netlify.com)
+
+# pixi-typescript-boilerplate
+
+Beginner friendly template for pixi.js with [Parcel](https://parceljs.org/)
+
+## Highlights
+
+-   🔰 Beginner friendly.
+-   🚀 Blazing fast bundle times
+-   🛠 Typescript.
+-   📝 Unit test support with [Jest](https://jestjs.io/), generating code coverage.
+-   ✈️ Live reload.
+-   📝 Consistent code style using Prettier and Eslint
+
+## Usage
+
+### Click on the green button "Use this template" on top of the repo or <br> <br> Simply copy paste this and start coding
+
+`git clone --depth=1 --branch=master https://github.com/jkanchelov/pixi-typescript-boilerplate`
+
+`rm -r -force ./pixi-typescript-boilerplate/.git`
+
+## Commands:
+
+-   `npm run build` - starts build procedure
+-   `npm run dev` - start watching for files and open's server on localhost:8080
+-   `npm run test` - run tests
+-   `npm run code-coverage` - generate code coverage report
+-   `npm run code-style-check` - run's eslint and prettier check on your code
+
+For vscode users - ctrl ( or ⌘ ) + shift + b will run the watch build as its set as default vscode task
---
+++ src/index.ts
@@ -0,0 +1,108 @@
+import * as PIXI from "pixi.js";
+
+import rabbitImage from "./assets/rabbit.png";
+
+export class Main {
+    private static readonly GAME_WIDTH = 800;
+    private static readonly GAME_HEIGHT = 600;
+
+    private app: PIXI.Application | undefined;
+
+    constructor() {
+        window.onload = (): void => {
+            this.startLoadingAssets();
+        };
+    }
+
+    // add for the test example purpose
+    public helloWorld(): string {
+        return "hello world";
+    }
+
+    private startLoadingAssets(): void {
+        const loader = PIXI.Loader.shared;
+        loader.add("rabbit", rabbitImage);
+        loader.add("spriteExample", "./spritesData.json"); // example of loading spriteSheet
+
+        loader.on("complete", () => {
+            this.onAssetsLoaded();
+        });
+        //
+        loader.load();
+    }
+
+    private onAssetsLoaded(): void {
+        this.createRenderer();
+
+        const stage = this.app!.stage;
+
+        const bunny = this.getBunny();
+        bunny.position.set(Main.GAME_WIDTH / 2, Main.GAME_HEIGHT / 2);
+
+        const birdFromSprite = this.getBird();
+        birdFromSprite.anchor.set(0.5, 0.5);
+        birdFromSprite.position.set(Main.GAME_WIDTH / 2, Main.GAME_HEIGHT / 2 + bunny.height);
+
+        stage.addChild(bunny);
+        stage.addChild(birdFromSprite);
+
+        this.app!.ticker.add(() => {
+            bunny.rotation += 0.05;
+        });
+    }
+
+    private createRenderer(): void {
+        this.app = new PIXI.Application({
+            backgroundColor: 0xd3d3d3,
+            width: Main.GAME_WIDTH,
+            height: Main.GAME_HEIGHT,
+        });
+
+        document.body.appendChild(this.app.view);
+
+        this.app.renderer.resize(window.innerWidth, window.innerHeight);
+        this.app.stage.scale.x = window.innerWidth / Main.GAME_WIDTH;
+        this.app.stage.scale.y = window.innerHeight / Main.GAME_HEIGHT;
+
+        window.addEventListener("resize", this.onResize.bind(this));
+    }
+
+    private onResize(): void {
+        if (!this.app) {
+            return;
+        }
+
+        this.app.renderer.resize(window.innerWidth, window.innerHeight);
+        this.app.stage.scale.x = window.innerWidth / Main.GAME_WIDTH;
+        this.app.stage.scale.y = window.innerHeight / Main.GAME_HEIGHT;
+    }
+
+    private getBunny(): PIXI.Sprite {
+        const bunnyRotationPoint = {
+            x: 0.5,
+            y: 0.5,
+        };
+
+        const bunny = new PIXI.Sprite(PIXI.Texture.from("rabbit"));
+        bunny.anchor.set(bunnyRotationPoint.x, bunnyRotationPoint.y);
+        bunny.scale.set(2, 2);
+
+        return bunny;
+    }
+
+    private getBird(): PIXI.AnimatedSprite {
+        const bird = new PIXI.AnimatedSprite([
+            PIXI.Texture.from("birdUp.png"),
+            PIXI.Texture.from("birdMiddle.png"),
+            PIXI.Texture.from("birdDown.png"),
+        ]);
+        bird.loop = true;
+        bird.animationSpeed = 0.1;
+        bird.play();
+        bird.scale.set(3);
+
+        return bird;
+    }
+}
+
+new Main();
